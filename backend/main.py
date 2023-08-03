d155de8eb4fa7474b44c42514d9d70dd6442341a
Nawreen Anan Khandaker <65958921+The-Talking-Head@users.noreply.github.com>
2023-08-03 17:23:00 +0600

    Day 2

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restx import Api, Resource, fields
 from config import DevConfig
-from models import Recipe
+from models import Recipe, User
 from exts import db
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, jwt_required
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
@@ -12,6 +14,8 @@ db.init_app(app)
 
 migrate=Migrate(app, db)
 
+app.config["JWT_SECRET_KEY"] = "secret" ##Must put a secret key
+JWTManager(app)
 
 api = Api(app, doc = '/docs')
 
@@ -25,21 +29,89 @@ recipe_model = api.model(
     }
 )
 
+
+signup_model=api.model(
+    'SignUp',
+    {
+        "username":fields.String(),
+        "email":fields.String(),
+        "password":fields.String()
+    }
+)
+
+login_model=api.model(
+    'Login',
+    {
+        "username": fields.String(),
+        "password": fields.String()
+    }
+)
+
 @api.route('/hello')
 class HelloResource(Resource):
     def get(self):
         return {"message": "Hello World"}
     
+@api.route('/signup')
+class SignUp(Resource):
+    # @api.marshal_with(signup_model)
+     #helps to serialize the objects in json and return it to us and tags the newly created user
+    @api.expect(signup_model)
+    def post(self):
+        data=request.get_json()
+
+        username=data.get('username') 
+        ##from the data we get the username and we check if the username already exists, if exits
+        ##then we dont create that user
+        
+        db_user=User.query.filter_by(username=username).first()
+
+        if db_user is not None:
+            return jsonify({"message":f"User with username {username} already exists"})
+
+
+        new_user=User(
+            username=data.get('username'),
+            email=data.get('email'),
+            ##We will hash the password and store the hash value in the database
+            password=generate_password_hash(data.get('password'))
+        )
+
+        new_user.save()
+        # return new_user, 201
+        return jsonify({"message": "User created successfully"})
+
+@api.route('/login')
+class Login(Resource):
+    @api.expect(login_model)
+    def post(self):
+        data=request.get_json()
+        username=data.get('username')
+        password=data.get('password')
+
+        db_user=User.query.filter_by(username=username).first() ##having the first object of the username
+
+        if db_user and check_password_hash(db_user.password, password):
+            access_token=create_access_token(identity=db_user.username)
+            refresh_token=create_refresh_token(identity=db_user.username)
+
+            return jsonify(
+                {"access_token": access_token, "refresh_token":refresh_token}
+            )
+
 
 @api.route('/recipes')
 class RecipesResource(Resource):
     @api.marshal_list_with(recipe_model)
+    @jwt_required()
     def get(self):
         """Get all recipes"""
         recipes=Recipe.query.all()
         return recipes
 
     @api.marshal_with(recipe_model)
+    @api.expect(recipe_model)
+    @jwt_required()
     def post(self):
         """Create a new recipe"""
         data = request.get_json()
@@ -62,6 +134,7 @@ class RecipeResource(Resource):
         return recipe
 
     @api.marshal_with(recipe_model)
+    @jwt_required()
     def put(self,id):
         """Update a recipe"""
         recipe_to_update=Recipe.query.get_or_404(id)
@@ -70,6 +143,7 @@ class RecipeResource(Resource):
         return recipe_to_update
 
     @api.marshal_with(recipe_model)
+    @jwt_required()
     def delete(self, id):
         """Delete a recipe by id"""
         recipe_to_delete=Recipe.query.get_or_404(id)
